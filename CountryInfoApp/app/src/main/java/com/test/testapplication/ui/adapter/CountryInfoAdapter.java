d4cf28fe267d5f05aef4434a1bfcd9d301028a22
moseskesavan <moses.kesavan@ril.com>
2017-11-21 18:29:47 +0530

    Initial Commit:
    Adding initial application structure.

---
+++ CountryInfoApp/app/src/main/java/com/test/testapplication/ui/adapter/CountryInfoAdapter.java
@@ -0,0 +1,29 @@
+package com.test.testapplication.ui.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.test.testapplication.ui.viewholder.CountryInfoViewHolder;
+
+/**
+ * Created by moseskesavan on 11/21/17.
+ *
+ * Class which binds the country info data to the UI.
+ */
+
+public class CountryInfoAdapter extends RecyclerView.Adapter<CountryInfoViewHolder> {
+    @Override
+    public CountryInfoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(CountryInfoViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
