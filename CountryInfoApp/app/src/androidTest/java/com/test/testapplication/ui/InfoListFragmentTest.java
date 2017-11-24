81aa686a6956c59f5039105572876023858d0552
moseskesavan <moses.kesavan@ril.com>
2017-11-24 18:27:35 +0530

    1. Adding UI instrumentation test case.

--- CountryInfoApp/app/src/androidTest/java/com/test/testapplication/ui/InfoListFragmentTest.java
+++ CountryInfoApp/app/src/androidTest/java/com/test/testapplication/ui/InfoListFragmentTest.java
@@ -5,6 +5,7 @@ import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.test.testapplication.R;
@@ -17,6 +18,7 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 
 /**
  * Created by moseskesavan on 11/24/17.
@@ -55,15 +57,32 @@ public class InfoListFragmentTest {
 
     @Test
     public void testListScroll() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         onView(withId(R.id.list_view))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(9, click()));
         try {
-            Thread.sleep(4000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         onView(withId(R.id.list_view))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+    }
+
+    @Test
+    public void testListHasData(){
+        if (getRVcount() > 0){
+            onView(withId(R.id.list_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        }
+    }
+
+    private int getRVcount(){
+        RecyclerView recyclerView = (RecyclerView) menuActivityTestRule.getActivity().findViewById(R.id.list_view);
+        return recyclerView.getAdapter().getItemCount();
     }
 
 }
